a56219fe71c6a02d86902b7561a233a55153f3b7
linquan.0807 <linquan.0807@bytedance.com>
2024-06-21 22:14:12 +0800

    finish the mvp android gradle demo
    Change-Id: I977b3743f8048feeb1ae86dc61612f84f39ef264

---
+++ plugin/src/main/java/com/test/greeting/MyTransform.java
@@ -0,0 +1,39 @@
+package com.test.greeting;
+
+
+import com.android.build.api.transform.QualifiedContent;
+import com.android.build.api.transform.Transform;
+import com.android.build.api.transform.TransformException;
+import com.android.build.api.transform.TransformInvocation;
+import com.android.build.gradle.internal.pipeline.TransformManager;
+
+import java.io.IOException;
+import java.util.Set;
+
+public class MyTransform extends Transform {
+    @Override
+    public String getName() {
+        return "MyTransform";
+    }
+
+    @Override
+    public Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS;
+    }
+
+    @Override
+    public Set<? super QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT;
+    }
+
+    @Override
+    public boolean isIncremental() {
+        return false;
+    }
+
+    @Override
+    public void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
+        super.transform(transformInvocation);
+        System.out.println("============== MyTransform Test ==============");
+    }
+}
